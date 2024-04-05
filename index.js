2a62f057af1c2e4f5d1b80823c966b6385d012a7
NGDEC03 <ngdec03@gmail.com>
2024-04-05 23:05:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ function creation(){
     spanContent.style.fontSize='22px'
     spanContent.style.wordBreak='break-word'
     spanContent.style.marginRight='50px'
-    spanImg.innerHTML="<span><img src='../assets/cross.svg' height='30px'></span>"
+    spanImg.innerHTML="<span><img src='../To-Do-List/assets/cross.svg' height='30px'></span>"
     spanImg.addEventListener('click',()=>{
         div.remove();
-        console.log(2);
         localStorage.setItem("data", document.querySelector('#main-section').innerHTML);
         // localStorage.removeItem()
     })
